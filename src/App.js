f23b16fab3269296b10541f6504226849f0e742f
ashutosh.kumar2 <ashutosh.kumar2@emids.com>
2023-04-18 18:09:38 +0530

    React State and Working with Event Handlers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import Expenses from './components/Expenses';
-function App() {
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/newExpense';
+const App = () => {
 
     const expenses = [
       {
@@ -25,8 +26,8 @@ function App() {
     
   return (
     <div>
-      <h2>Expense Tracker</h2>
-        <Expenses expenses = {expenses} />
+      <NewExpense />
+      <Expenses expenses = {expenses} />
     </div>
   );
 }
