40d0823f1f3027270eef61f64da5322eaa510537
ashutosh.kumar2 <ashutosh.kumar2@emids.com>
2023-04-20 12:17:36 +0530

    Conditional rendering on the Expenses comp.

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense = {addExpenseHandler}/>
-      <Expenses expenses = {expenses} />
+      <Expenses expenses = {expenses}/>
     </div>
   );
 }
